5b28ee12437e6ce7776de9104cc707b6e7bf37b6
edsontomas54 <donthimas54@gmail.com>
2023-01-16 19:23:31 +0200

    Details product added completed at LSN 06

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\ProfileController;
 use App\Http\Livewire\Admin\AdminDashboardComponent;
 use App\Http\Livewire\CartComponent;
 use App\Http\Livewire\CheckoutComponent;
+use App\Http\Livewire\DetailsComponent;
 use App\Http\Livewire\HomeComponent;
 use App\Http\Livewire\ShopComponent;
 use App\Http\Livewire\User\UserDashboardComponent;
@@ -32,18 +33,19 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-
 });
 Route::get('/', HomeComponent::class)->name('home.index');
-    Route::get('/shop', ShopComponent::class)->name('shop');
-    Route::get('/cart', CartComponent::class)->name('shop.cart');
-    Route::get('/checkout', CheckoutComponent::class)->name('shop.checkout');
+Route::get('/shop', ShopComponent::class)->name('shop');
+Route::get('/cart', CartComponent::class)->name('shop.cart');
+Route::get('/checkout', CheckoutComponent::class)->name('shop.checkout');
 
-Route::middleware(['auth'])->group(function(){
+Route::get('/product/{slug}',DetailsComponent::class)->name('product.details');
+
+Route::middleware(['auth'])->group(function () {
     Route::get('/user/dashboard', UserDashboardComponent::class)->name('user.dashboard');
 });
 
-Route::middleware('auth', 'authadmin')->group(function(){
+Route::middleware('auth', 'authadmin')->group(function () {
     Route::get('/admin/dashboard', AdminDashboardComponent::class)->name('admin.dashboard');
 });
 
